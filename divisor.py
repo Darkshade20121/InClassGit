68d1d0c3ef34b49a5444686881186ca5bf5f4641
andrewkassab7@gmail.com <>
2021-04-15 17:29:56 -0700

    if statement for 0

--- divisor.py
+++ divisor.py
@@ -3,6 +3,8 @@ import math
 def divisor(number):
 
     for i in range (1, int(math.sqrt(number)+1)):
+        if(number == 0):
+
         if (number % i == 0):
             if number / i != i:
                     print(i)
