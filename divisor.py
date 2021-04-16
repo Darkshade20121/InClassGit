4b0b8beda39c80ccf132470ece934434bc97ab5c
andrewkassab7@gmail.com <>
2021-04-15 17:23:25 -0700

    for loop creation for divisor

--- divisor.py
+++ divisor.py
@@ -2,3 +2,12 @@ import math
 
 def divisor(number):
 
+    for i in range (1, int(math.sqrt(number)+1)):
+        if (number % i == 0):
+            if number / i != i:
+                    print(i)
+                    print(number / i)
+            else:
+                print(i)
+   
+
