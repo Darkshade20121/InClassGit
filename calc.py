cd6958f3c55b0f780f5a47928aea8f464d905c2a
andrewkassab7@gmail.com <>
2021-04-15 17:00:05 -0700

    for loop to add elements together of list and print the sum

--- calc.py
+++ calc.py
@@ -6,7 +6,9 @@ def calc(a, b):
 
     my_list = [sum, difference, multiplication, division]
 
-   
+    for x in range(0, len(my_list)):
+        sum = sum + my_list[x]
 
+    print(sum)
 
 calc(3,4)
