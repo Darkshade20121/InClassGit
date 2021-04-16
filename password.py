4c33d6495fc4b42ab86a4b724db6f66b485e0674
andrewkassab7@gmail.com <>
2021-04-15 17:52:14 -0700

    second for loop to print out the list

--- password.py
+++ password.py
@@ -4,4 +4,7 @@ def password(num):
     password = []
     for i in range (0,num):
         password.append(random.choice(chars))
+        
+    for i in range(len(password)):
+        print(password[i])
     
