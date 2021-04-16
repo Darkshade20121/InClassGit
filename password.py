a111912a44e08cf62a02e8fe773bddbb094cdb52
andrewkassab7@gmail.com <>
2021-04-15 17:59:09 -0700

    Finished product with both forloops working print out a password

--- password.py
+++ password.py
@@ -1,10 +1,16 @@
 import random
+import string
 
 def password(num):
     password = []
+    letters = string.ascii_letters
+
     for i in range (0,num):
-        password.append(random.choice(chars))
+        password.append(random.choice(letters))
         
     for i in range(len(password)):
-        print(password[i])
+        print(password[i],end='')
+        
+
+password(5)
     
